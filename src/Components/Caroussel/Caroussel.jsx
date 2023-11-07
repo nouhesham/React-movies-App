f5b186789f336f44ea901f948d67ddfd4b262a5d
Nourhan hesham <nourhanhesham263@gmail.com>
2023-11-07 12:31:00 +0200

    adding styles

--- src/Components/Caroussel/Caroussel.jsx
+++ src/Components/Caroussel/Caroussel.jsx
@@ -1,30 +1,18 @@
 import Carousel from "react-bootstrap/Carousel";
-// import ExampleCarouselImage from "components/ExampleCarouselImage";
 
-function CarouselFadeExample() {
+function CarouselFadeExample({ movies }) {
+  const limitedMovies = movies?.slice(0, 3);
   return (
     <Carousel fade>
-      <Carousel.Item>
-        <img
-          style={{ width: "100%" }}
-          src="https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/bMRofddQE58ToKM7GtdJy6MuKoY.jpg"
-          alt={1}
-        ></img>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ width: "100%" }}
-          src="https://image.tmdb.org/t/p/w1920_and_h800_multi_faces//eF6pkcpzxDCaQnJhaRlKdhhQebB.jpg"
-          alt={2}
-        ></img>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ width: "100%" }}
-          src="https://image.tmdb.org/t/p/w1920_and_h800_multi_faces//628Dep6AxEtDxjZoGP78TsOxYbK.jpg"
-          alt={3}
-        ></img>
-      </Carousel.Item>
+      {limitedMovies?.map((movie) => (
+        <Carousel.Item key={movie.id}>
+          <img
+            style={{ width: "100%" }}
+            src={`https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${movie.backdrop_path}`}
+            alt={movie.title}
+          />
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import Movies from "../Movies/Movies";
 import InputGroup from "../InputGroup/InputGroup";
 import CarouselFadeExample from "../Caroussel/Caroussel";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const moviesList = useSelector((store) => store.movies.moviesList);
+
   return (
     <div>
-      <CarouselFadeExample />
+      <CarouselFadeExample movies={moviesList} />
       <InputGroup />
       <Movies />
     </div>
