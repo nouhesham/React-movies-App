c05befa5437af3be1ce104ce70d2c6dbcadf3bcf
Nourhan hesham <nourhanhesham263@gmail.com>
2023-11-11 16:56:33 +0200

    adding styles

--- src/Components/MovieDetails/MovieDetails.jsx
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -27,7 +27,7 @@ const MovieDetails = () => {
   const percentage = Math.floor(movie.vote_average * 10);
 
   return (
-    <div>
+    <div style={{ height: "100vh" }}>
       <div
         style={{
           backgroundImage: `url("https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${movie.backdrop_path}")`,
